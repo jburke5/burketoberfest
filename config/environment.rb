f0d7a03150c3e619ceece85a3167d317d7025117
acslocum <acslocum@thoughtworks.com>
2013-10-26 13:10:34 -0400

    correctly setting time zone

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-config.time_zone = "Eastern Time (US & Canada)"
-
 # Initialize the Rails application.
 Burketoberfest::Application.initialize!
